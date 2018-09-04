c9863435ed2ce4363f57fb98cbd16fe26fb5403a
AsJocker <42966092+AsJocker@users.noreply.github.com>
2018-09-04 13:48:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg = require('./index.json');
 const prefix = ("!");
-const token = process.env.token
+const token = process.env.token;
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
